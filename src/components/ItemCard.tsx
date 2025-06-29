359ea4a408efcb9a185e36e6999817f1cfe803e7
nuonvannak <nuonvannaksingle@gmail.com>
2025-06-30 06:32:46 +0700

    Initial project push

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -12,6 +12,10 @@ export default function ItemCard({
   imageAlt,
 }: ItemCardProps) {
   const { t } = useTranslation();
+  const addCard = (id: number, category: string) => () => {
+    console.log(`Adding item with id: ${id} and category: ${category}`);
+  };
+
   return (
     <div className="food-card mb-5 dark:bg-b-normal dark:text-t-text">
       <img
@@ -20,13 +24,23 @@ export default function ItemCard({
         className="w-28 h-28 mx-auto rounded-full -mt-12 mb-4 object-cover border-4 border-white"
       />
       <div className="flex flex-col gap-2">
-      <h3 className="text-lg font-bold">{name}</h3>
-      <h4 className="text-sm font-bold">{t(status)}</h4>
+        <h3 className="text-lg font-bold">{name}</h3>
+        <h4 className="text-sm font-bold">{t(status)}</h4>
       </div>
       <div className="flex justify-between items-center mt-4">
         <p className="text-red-300 text-xl font-extrabold">{price}$</p>
-        <button className="bg-red-500 text-white w-8 h-8 rounded-full hover:bg-red-600 transition-colors">
-          +
+        <button
+          onClick={addCard(id, category)}
+          className="bg-red-500 flex justify-center items-center text-white w-8 h-8 rounded-full hover:bg-red-600 transition-colors"
+        >
+          <svg width="20" height="20" viewBox="0 0 20 20" fill="none">
+            <path
+              d="M10 4v12M4 10h12"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+            />
+          </svg>
         </button>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,14 @@ module.exports = {
         khmer: ["Khmer OS Battambang", "Khmer OS", "Khmer", "sans-serif"],
       },
       colors: {
-        "t-text":"#98a0ae",
-        "t-normal":"#9ba2ae",
-        "b-bg":"#111826",
-        "b-normal":"#1e2939",
+        "t-text": "#98a0ae",
+        "t-normal": "#9ba2ae",
+        "b-bg": "#111826",
+        "b-normal": "#1e2939",
+      },
+      boxShadow: {
+        "glow-dark": "0 0 15px rgba(255, 255, 255, 0.15)",
+        "glow-dark-hover": "0 0 25px rgba(255, 255, 255, 0.15)",
       },
     },
   },
